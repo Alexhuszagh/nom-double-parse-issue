933b1eb5deb385c25a14641679c1cb890d31d3e3
Alex Huszagh <ahuszagh@gmail.com>
2020-01-07 17:29:24 -0600

    Updated the examples for more sample pieces of code.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,17 @@ use lexical_043;
 use lexical_070;
 
 fn main() {
-    println!("nom double parse: {:?}", nom_double(". "));
-    println!("lexical_043 double parse: {:?}", lexical_043_double(". "));
-    println!("lexical_070 double parse: {:?}", lexical_070_double(". "));
+    let values = [
+        ".",
+        ". ",
+        ".1 "
+    ];
+    for value in values.iter() {
+        println!("Parsing value: '{:?}'", value);
+        println!("  nom double parse: {:?}", nom_double(value));
+        println!("  lexical_043 double parse: {:?}", lexical_043_double(value));
+        println!("  lexical_070 double parse: {:?}", lexical_070_double(value));
+    }
 }
 
 fn lexical_043_double(input: &str) -> lexical_043::Result<f64> {
